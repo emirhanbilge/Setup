ca0d0762b2d13e5db64de71bd5d44fe1092d235f
Emirhan Bilge Bulut <36032629+emirhanbilge@users.noreply.github.com>
2021-09-09 17:46:31 +0300

    Add files via upload

--- network.sh
+++ network.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 
-param($1 , $2 , $3)
+#param($1 , $2 , $3)
 
-echo "auto eth0" |  sudo nano /etc/network/interfaces
-echo "iface eth0 inet dhcp" |  sudo nano /etc/network/interfaces
+echo 1234 | sudo -S su
 
 
-echo "auto eth0" | sudo nano /etc/network/interfaces
-echo "iface eth0" inet static | sudo nano /etc/network/interfaces
-echo "address"+ $1 | sudo nano /etc/network/interfaces
+echo "auto eth0" >>  /etc/network/interfaces
+echo "iface eth0 inet dhcp" >>  /etc/network/interfaces
 
-echo "netmask " + $3 | sudo nano /etc/network/interfaces
-echo "gateway"+ $2 | sudo nano /etc/network/interfaces 
-echo  "dns-nameservers 4.4.4.4" | sudo nano /etc/network/interfaces 
-echo  "dns-nameservers 8.8.8.8"| sudo nano /etc/network/interfaces
+
+echo "auto eth0" >>   /etc/network/interfaces
+echo "iface eth0" >> /etc/network/interfaces
+echo "inet static" >>  /etc/network/interfaces
+echo "address {$1}"  >>  /etc/network/interfaces
+
+echo "ip address" 
+echo $1
+
+echo "gateway"
+echo $2
+
+echo "netmask"
+echo $3
+
+
+echo "" >   /etc/network/interfaces
+echo "netmask {$3} "  >>   /etc/network/interfaces
+echo "gateway {$2}"  >>   /etc/network/interfaces 
+echo  "dns-nameservers 4.4.4.4" >>  /etc/network/interfaces 
+echo  "dns-nameservers 8.8.8.8" >>  /etc/network/interfaces
 
 echo "static" >  conf.txt
--- networkRunner.py
+++ networkRunner.py
@@ -5,16 +5,20 @@ import subprocess
 
 def sendNetwork(ip , subnetMask , gateway ):
 
-
-
-    bashCommand = "chmod u+x network.sh " 
+    bashCommand = "echo 1234 | sudo -S su"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    
+    
+    bashCommand = "sudo chmod u+x network.sh " 
 
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
 
     print("Error code " , error)
 
-    bashCommand = "./network.sh" +  ip +" "+ subnetMask +" "+ gateway
+    bashCommand = "sudo ./network.sh " +  ip +" "+ subnetMask +" "+ gateway
 
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
